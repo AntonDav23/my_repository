d862d980d3e9f8182e28138294c7d1d4cd35709b
Anton Davydov <a.davydov@ro.ru>
2026-04-23 17:30:30 +0300

    'restore_order'

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,20 @@
-def mask_account_card(proces_inform: str) -> str:
-    ''' Функция обрабатывает информацию как о картах, так и о счетах '''
-    len_list = len(proces_inform)
+def mask_account_card(process_info: str) -> str:
+    """Функция обрабатывает информацию как о картах, так и о счетах"""
+    len_list = len(process_info)
     if len_list == 30:
-        mask_numbers = f"{proces_inform[:4]} {proces_inform[5:13]} {proces_inform[14:18]} {proces_inform[19:21]} ** ***** {proces_inform[-4:]}"
+        mask_numbers = f"{process_info[:4]} {process_info[5:13]} {process_info[14:18]} {process_info[19:21]} ** ***** {process_info[-4:]}"
     if len_list == 24:
-        mask_numbers = f"{proces_inform[:7]} {proces_inform[8:12]} {proces_inform[13:15]} ** **** {proces_inform[-4:]}"
+        mask_numbers = f"{process_info[:7]} {process_info[8:12]} {process_info[13:15]} ** **** {process_info[-4:]}"
     if len_list == 25:
-        mask_numbers = f"{proces_inform[:5]} ** {proces_inform[-4:]}"
+        mask_numbers = f"{process_info[:5]} ** {process_info[-4:]}"
     return mask_numbers
 
 
-def get_date(formatting_date: str) -> str:
-    '''  Функция меняет формат даты '''
-    data = formatting_date[:10]
+def get_date(format_date: str) -> str:
+    """Функция меняет формат даты"""
+    data = format_date[:10]
     new_data = []
-    for element in data.split('-'):
+    for element in data.split("-"):
         new_data.append(element)
         reversed_new_data = new_data[::-1]
-    return '.'.join(reversed_new_data)
+    return ".".join(reversed_new_data)
