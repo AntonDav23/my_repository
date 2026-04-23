c986a975d60be6450f73b7facb8a0820314c7375
Anton Davydov <a.davydov@ro.ru>
2026-04-23 09:36:17 +0300

    'add_function_format_date'

--- src/widget.py
+++ src/widget.py
@@ -8,3 +8,13 @@ def mask_account_card(proces_inform: str) -> str:
     if len_list == 25:
         mask_numbers = f"{proces_inform[:5]} ** {proces_inform[-4:]}"
     return mask_numbers
+
+
+def get_date(formatting_date: str) -> str:
+    '''  Функция меняет формат даты '''
+    data = formatting_date[:10]
+    new_data = []
+    for element in data.split('-'):
+        new_data.append(element)
+        reversed_new_data = new_data[::-1]
+    return '.'.join(reversed_new_data)
