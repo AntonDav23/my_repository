fdc70e57bf52702a7b5b1d2c2432a81950bd23e9
AntonDav23 <antonses@icloud.com>
2026-04-25 10:08:09 +0300

    'fixed_the_function_mask_account_card_and_import_masks'

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,16 @@
-def mask_account_card(process_info: str) -> str:
+from masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(some_info: str) -> str:
     """Функция обрабатывает информацию как о картах, так и о счетах"""
-    len_list = len(process_info)
-    if len_list == 30:
-        mask_numbers = f"{process_info[:4]} {process_info[5:13]} {process_info[14:18]} {process_info[19:21]} ** ***** {process_info[-4:]}"
-    if len_list == 24:
-        mask_numbers = f"{process_info[:7]} {process_info[8:12]} {process_info[13:15]} ** **** {process_info[-4:]}"
-    if len_list == 25:
-        mask_numbers = f"{process_info[:5]} ** {process_info[-4:]}"
-    return mask_numbers
+    part_name = some_info.split()
+    number = part_name[-1]
+    together = " ".join(part_name[:-1])
+    if together.upper() == "Счет":
+        masked_number = get_mask_account(number)
+    else:
+        masked_number = get_mask_card_number(number)
+    return f"{together}{masked_number}"
+
 
 from datetime import datetime
 
