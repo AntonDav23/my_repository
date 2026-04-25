1ee7c051215d7c7672019b92263c6571c039f991
AntonDav23 <antonses@icloud.com>
2026-04-25 22:42:35 +0300

    'fixed_

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,22 @@
-from masks import get_mask_account, get_mask_card_number
+import re
+from masks import get_mask_card_number, get_mask_account
 
 
-def mask_account_card(process_info: str) -> str:
+def mask_account_card(input_str: str) -> str:
     """Функция обрабатывает информацию как о картах, так и о счетах"""
-    part_name = process_info.split()
-    number = part_name[-1]
-    together = " ".join(part_name[:-1])
-    if together.upper() == "Счет":
-        masked_number = get_mask_account(number)
-    else:
-        masked_number = get_mask_card_number(number)
-    return f"{together}{masked_number}"
+    card_match = re.search(r'\b\d{16}\b', input_str)
+
+    if card_match:
+        card_number = card_match[0]
+        masked_card = get_mask_card_number(card_number)
+        return input_str.replace(card_number, masked_card)
+
+    account_match = re.search(r'Счет (\d+)', input_str)
+
+    if account_match:
+        account_number = account_match[0]
+        masked_account = get_mask_account(account_number)
+        return input_str.replace(account_number, masked_account)
 
 
 from datetime import datetime
