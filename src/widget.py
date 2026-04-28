4ae4717f2ed01761922c53adfd01299e6276a1b8
AntonDav23 <antonses@icloud.com>
2026-04-28 22:59:19 +0300

    'created_module_processing'

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 from masks import get_mask_account, get_mask_card_number
 
@@ -27,18 +28,7 @@ def mask_account_card(input_str: str) -> str:
     return input_str
 
 
-from datetime import datetime
-
-
 def get_date(format_date: str) -> str:
     """Функция меняет формат даты"""
     date_object = datetime.strptime(format_date, "%Y-%m-%dT%H:%M:%S.%f")
     return date_object.strftime("%d.%m.%Y")
-
-if __name__ == "__main__":
-    result_card = mask_account_card("Visa Platinum 7000792289606361")
-    print(result_card)
-    # Выведет: Visa Platinum 7000 79** **** 6361 (Верно!)
-
-    result_account = mask_account_card("Счет 73654108430135874305")
-    print(result_account)
