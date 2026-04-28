f001f70632e6bf804b3d6f47528a3ca022ad771e
AntonDav23 <antonses@icloud.com>
2026-04-29 00:13:08 +0300

    'code_quality_control'

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,10 @@
-def filter_by_state(items: list[dict], state_value: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(items: list[dict], state_value: str = "EXECUTED") -> list[dict]:
     """Функция, которая фильтрует список словарей по значению ключа 'state'"""
-    return [item for item in items if item.get('state') == state_value]
+
+    return [item for item in items if item.get("state") == state_value]
 
 
-def sort_by_date(items, reverse=True):
+def sort_by_date(items: "list[dict]", reverse: bool = True) -> "list[dict]":
     """Функция, которая сортирует список словарей по ключу 'date''"""
-    return sorted(items, key=lambda x: x['date'], reverse=reverse)
+
+    return sorted(items, key=lambda x: x["date"], reverse=reverse)
