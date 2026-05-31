a2eed85d728d3d7a0538b326ddaa8d7104f11cec
AntonDav23 <antonses@icloud.com>
2026-05-31 20:35:52 +0300

    'making_changes_utils_module_according_mentor_remark'

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
 import json
-from typing import Dict, List
+from typing import List, Dict
 
 
 def load_transactions_from_json(file_path: str) -> List[Dict]:
-    """Загружает данные о транзакциях из JSON-файла."""
+    """
+    Загружает данные о транзакциях из JSON-файла."""
     try:
-        with open(file_path, "r", encoding="utf-8") as f:
+        with open(file_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
-            # Проверяем, что данные являются списком
             return data if isinstance(data, list) else []
-    except FileNotFoundError, json.JSONDecodeError:
+    except (FileNotFoundError, json.JSONDecodeError):
+
         return []
