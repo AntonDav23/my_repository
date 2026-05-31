05c4994fd5e2bcbd3706e9313c6b4d0ded4dfb85
AntonDav23 <antonses@icloud.com>
2026-05-31 22:49:53 +0300

    'made_order'

--- src/masks.py
+++ src/masks.py
@@ -3,19 +3,16 @@ import os
 
 # НАСТРОЙКА ЛОГГЕРА ДЛЯ МОДУЛЯ masks
 
-log_dir = os.path.join(os.path.dirname(__file__), '..', 'logs')
+log_dir = os.path.join(os.path.dirname(__file__), "..", "logs")
 os.makedirs(log_dir, exist_ok=True)
 
-logger = logging.getLogger('src.masks')
+logger = logging.getLogger("src.masks")
 logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler(os.path.join(log_dir, 'masks.log'), mode='w')
+file_handler = logging.FileHandler(os.path.join(log_dir, "masks.log"), mode="w")
 file_handler.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
